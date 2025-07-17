16cb85a2875032c6aa445035568297a3a33c3dc3
harhit-1000 <harshitredcolour@gmail.com>
2025-07-17 09:58:25 +0530

    Save

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import taskRoutes from './routes/taskRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import { protect } from './middleware/authMiddleware.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import taskRoutes from "./routes/taskRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 // router.use(protect);
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks',protect, taskRoutes);
+app.get("/", (req, res) => res.send("API is running..."));
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", protect, taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('MongoDB connected');
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
